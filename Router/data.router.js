6d89cc55cffdaa2bb6b92a38629e703b40772b2f
uttamkrshaw <uttamkr5599@gmail.com>
2023-06-08 00:52:11 +0530

    updated post request

--- Router/data.router.js
+++ Router/data.router.js
@@ -13,8 +13,30 @@ dataRouter.get('/get', async (req, res) => {
 
 dataRouter.post('/add', async (req, res) => {
     try {
-        console.log("payload",req.params)
-        res.send(200).send("Testing")
+        const {
+            First_Name,
+            Last_Name,
+            Email,
+            Country,
+            State,
+            City,
+            Gender,
+            Date_of_Birth,
+            Age
+        } = req.body
+        const user = new DataModel({
+            First_Name,
+            Last_Name,
+            Email,
+            Country,
+            State,
+            City,
+            Gender,
+            Date_of_Birth,
+            Age
+        })
+        await user.save()
+        res.send(200).send("User Added In The Database")
     } catch (error) {
         res.status(400).send(error)
     }
